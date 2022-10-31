9dbe3384abe166b1fe045a4d73ce227b45ed0438
LoonyJava <loony.java@gmail.com>
2022-11-01 01:30:30 +0500

    Final Task

--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -2,6 +2,22 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
+        double maxSide = 0;
+        if (firstSide > secondSide && firstSide > thirdSide) {
+            maxSide = firstSide;
+        } else if (secondSide > firstSide && secondSide > thirdSide) {
+            maxSide = secondSide;
+        } else maxSide = thirdSide;
 
+        if (firstSide <= 0 || secondSide <= 0 || thirdSide <= 0) {
+            System.out.println("it's not a triangle");
+        } else if (maxSide == thirdSide && (firstSide + secondSide) > maxSide) {
+            System.out.println("this is a valid triangle");
+        } else if (maxSide == secondSide && (firstSide + thirdSide) > maxSide) {
+            System.out.println("this is a valid triangle");
+        } else if (maxSide == firstSide && (secondSide + thirdSide) > maxSide) {
+            System.out.println("this is a valid triangle");
+        } else System.out.println("it's not a triangle");
     }
 }
+
